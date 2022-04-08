45e8738d7b71cb68002fe3fe367395e9842f4204
jesse zenon <jesse.zenon@gmail.com>
2022-04-08 11:17:36 -0500

    Done

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+const currentLocation = 42;
+function distanceFromHqInBlocks(pickupLocation) {
+  return Math.abs(pickupLocation - currentLocation);
+}
+
+function distanceFromHqInFeet(pickupLocation) {
+  return Math.abs(distanceFromHqInBlocks(pickupLocation) * 264);
+}
+
+function distanceTravelledInFeet(start, destination) {
+  return Math.abs((start - destination) * 264);
+}
+
+function calculatesFarePrice(start, destination) {
+  let blah = distanceTravelledInFeet(start, destination);
+  if (blah <= 400) {
+    return 0;
+  } else if (blah <= 2000) {
+    return 0.02 * (blah - 400);
+  } else if (blah <= 2500) {
+    return 25;
+  } else if (blah > 2500) {
+    return "cannot travel that far";
+  }
+}
