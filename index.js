ce41fcd392abd1c2442fb83532608867952367b5
jesse zenon <jesse.zenon@gmail.com>
2022-04-25 15:05:06 -0500

    commit tree b8cacdeb588062b233705081ac0c355e6087d372

--- index.js
+++ index.js
@@ -1,26 +1,58 @@
-// Code your solution in this file!
-const currentLocation = 42;
-function distanceFromHqInBlocks(pickupLocation) {
-  return Math.abs(pickupLocation - currentLocation);
+// // Code your solution in this file!
+// const currentLocation = 42;
+// function distanceFromHqInBlocks(pickupLocation) {
+//   return Math.abs(pickupLocation - currentLocation);
+// }
+
+// function distanceFromHqInFeet(pickupLocation) {
+//   return Math.abs(distanceFromHqInBlocks(pickupLocation) * 264);
+// }
+
+// function distanceTravelledInFeet(start, destination) {
+//   return Math.abs((start - destination) * 264);
+// }
+
+// function calculatesFarePrice(start, destination) {
+//   let blah = distanceTravelledInFeet(start, destination);
+//   if (blah <= 400) {
+//     return 0;
+//   } else if (blah <= 2000) {
+//     return 0.02 * (blah - 400);
+//   } else if (blah <= 2500) {
+//     return 25;
+//   } else if (blah > 2500) {
+//     return "cannot travel that far";
+//   }
+// }
+
+const scuberHq = 42;
+
+function distanceFromHqInBlocks(num) {
+  return Math.abs(scuberHq - num);
 }
 
-function distanceFromHqInFeet(pickupLocation) {
-  return Math.abs(distanceFromHqInBlocks(pickupLocation) * 264);
-}
-
-function distanceTravelledInFeet(start, destination) {
-  return Math.abs((start - destination) * 264);
-}
+const distanceFromHqInFeet = (num) => {
+  return Math.abs(scuberHq - num) * 264;
+};
 
 function calculatesFarePrice(start, destination) {
-  let blah = distanceTravelledInFeet(start, destination);
-  if (blah <= 400) {
+  let fuck = distanceTravelledInFeet(start, destination);
+
+  if (fuck < 400) {
     return 0;
-  } else if (blah <= 2000) {
-    return 0.02 * (blah - 400);
-  } else if (blah <= 2500) {
+  } else if (fuck > 400 && fuck < 2000) {
+    return (fuck - 400) * 0.02;
+  } else if (fuck > 2000 && fuck < 2500) {
     return 25;
-  } else if (blah > 2500) {
+  } else if (fuck > 2500) {
     return "cannot travel that far";
   }
 }
+
+const distanceTravelledInFeet = (start, destination) => {
+  if (start > destination) {
+    return (start - destination) * 264;
+  } else {
+    return (destination - start) * 264;
+  }
+};
